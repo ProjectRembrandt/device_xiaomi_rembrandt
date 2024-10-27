b969e88699aab7b6e88d56d6c849d11b106f9b52
mmqmemm <rdgbnirr3418435@163.com>
2024-10-27 14:45:54 +0800

    rembrandt

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# device_xiaomi_rembrandt
+Copyright (C) 2024 The LineageOS Project
+
+Device configuration for Redmi K60E
+=============================================================
+
+The Redmi K60E (codenamed _"rembrandt"_) is an mid-range smartphone from Xiaomi.
+
+It was announced and released in December 2022.
+
+## Device specifications
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+SoC     | Mediatek Dimensity 8200 (MT6896)
+CPU     | Octa-core (4x3.10 GHz Cortex-A78 & 4x2.0 GHz Cortex-A55)
+GPU     | Mali-G610 MC6
+Memory  | 8/12GB, LPDDR5
+Storage | 256/512GB, UFS 3.1
+Shipped Android Version | 12 with MIUI 13
+Battery | Non-removable Li-Po 5500 mAh battery
+Display | AMOLED, 6.67 inches, 1440 x 3200 pixels
+
+## Device Picture
+
+![Redmi K60E](https://cdn.cnbj0.fds.api.mi-img.com/b2c-shopapi-pms/pms_1672037146.81276139.png)
---
+++ device.mk
@@ -0,0 +1,73 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# IFAAService
+PRODUCT_PACKAGES += \
+    IFAAService
+
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/uinput-fpc.idc \
+    $(LOCAL_PATH)/configs/idc/uinput-goodix.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/uinput-goodix.idc \
+    $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
+# Fingerprint
+TARGET_HAS_UDFPS := false
+
+# NFC
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    Tag
+
+PRODUCT_PACKAGES += \
+    android.hardware.nfc-service.nxp
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.ese.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
+    frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
+    frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
+
+# Overlay
+PRODUCT_PACKAGES += \
+    FrameworksResOverlayRembrandt \
+    SettingsProviderOverlayRembrandt \
+    SettingsResOverlayRembrandt \
+    SystemUIOverlayRembrandt \
+    WifiResOverlayRembrandt
+
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.project.rc
+
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
+# Soong
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Shipping API Level
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+# Inherit from mt6895-common
+$(call inherit-product, device/xiaomi/mt6895-common/mt6895.mk)
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/xiaomi/rembrandt/rembrandt-vendor.mk)
